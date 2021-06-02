a8a17725e512be95f88d6ba6ab2f07d88d56db03
yandex-praktikum <58176914+yandex-praktikum@users.noreply.github.com>
2021-06-02 12:00:52 +0300

    fixed tests launch

--- test_program.py
+++ test_program.py
@@ -5,10 +5,16 @@ dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
 
 files_list = ['program.py', 'readme.md']
 
-for filename in files_list:
-    assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
 
-try:
-    import program
-except Exception as e:
-    assert False, 'Не удалось запустить `program.py`. Исправьте в нем ошибки.'
+def test_program():
+    for filename in files_list:
+        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
+
+    try:
+        import program
+    except Exception as e:
+        assert False, (
+            'Не удалось запустить `program.py`. '
+            'Исправьте в нем ошибки:\n'
+            f'{e}'
+        )
