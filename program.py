e26a3c4126ec6577fa5aa54c89ea4dd99f6543a3
TominAndrey <tominfarmed@yandex.ru>
2023-04-10 21:51:10 +0300

    changed doc str

--- program.py
+++ program.py
@@ -90,7 +90,7 @@ class SportsWalking(Training):
 
 @dataclass
 class Swimming(Training):
-    """Тренировка: плавание."""
+    """Тренировка: ."""
     length_pool: float
     count_pool: int
     LEN_STEP: InitVar[float] = 1.38
